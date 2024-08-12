2bbe849fe64ddf7930f1ee5b62e76c05a147651a
Jiwoong <dnd0311@naver.com>
2024-08-12 13:35:55 +0900

    login process #1,2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoginUserDto } from '../user/dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,12 @@ export class AuthController {
   @Post('/signup')
   async signupUser(@Body() createUserDto: CreateUserDto) {
     return await this.authService.signupUser(createUserDto);
+  }
+
+  @Post('/login')
+  async loginUser(@Body() loginUserDto: LoginUserDto) {
+    const user = await this.authService.loginUser(loginUserDto);
+    const token = await this.authService.generateAccessToken(user.id);
+    return { user, token };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoginUserDto } from '../user/dto/login-user.dto';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async signupUser(createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const user = await this.userService.getUserByEmail(loginUserDto.email);
+
+    const isMatched = await bcrypt.compare(
+      loginUserDto.password,
+      user.password,
+    );
+    if (!isMatched) {
+      throw new HttpException('Password do not match', HttpStatus.BAD_REQUEST);
+    }
+    return user;
+  }
+
+  public generateAccessToken(userId: string) {
+    const payload: any = { userId };
+    const token = this.jwtService.sign(payload, {
+      secret: this.configService.get('ACCESS_TOKEN_SECRET'),
+      expiresIn: this.configService.get('ACCESS_TOKEN_EXPIRATION_TIME'),
+    });
+    return token;
   }
 }
